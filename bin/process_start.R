504e255e16731b67d647599a30622bc03be276e1
Jack Scanlan <jacksca@gmail.com>
2024-03-05 16:29:58 +1100

    .

--- bin/process_start.R
+++ bin/process_start.R
@@ -1,9 +1,20 @@
-# source("${projectDir}/jack_notes/.Rprofile")
-# source("${projectDir}/bin/functions.R")
-# source("${projectDir}/bin/themes.R")
-# source("${projectDir}/bin/_targets_packages.R")
+##### this loads the listed R scripts 
 
-source(file.path(projectDir, "jack_notes/.Rprofile"))
-source(file.path(projectDir, "bin/functions.R"))
-source(file.path(projectDir, "bin/themes.R"))
-source(file.path(projectDir, "bin/_targets_packages.R"))
+# "projectDir" needs to be defined in the 
+
+script_list <-
+    c(
+        "jack_notes/.Rprofile",
+        "bin/functions.R",
+        "bin/themes.R",
+        "bin/_targets_packages.R"
+    )
+
+for (script in script_list) {
+    source(file.path(projectDir, script))
+}
+
+# source(file.path(projectDir, "jack_notes/.Rprofile"))
+# source(file.path(projectDir, "bin/functions.R"))
+# source(file.path(projectDir, "bin/themes.R"))
+# source(file.path(projectDir, "bin/_targets_packages.R"))
---
+++ bin/seq_qc.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+tar_target(seq_qc, {
+            process <- temp_samdf1_grouped %>%
+            dplyr::group_by(fcid) %>%
+            tidyr::nest() %>%
+            dplyr::mutate(seq_qc = purrr::map(fcid, step_seq_qc, quiet=FALSE, write_all=FALSE))
+            out <- paste0("output/logs/", unique(process$fcid),"/",unique(process$fcid),"_flowcell_qc.pdf")
+            if(is.na(process$seq_qc[[1]]$reads_pf)){
+            pdf(file=out, paper="A4")
+            plot.new()
+            text(x=.5, y=.5, "ERROR: InterOp folder or RunInfo.xml not present") 
+            try(dev.off(), silent=TRUE)
+            }
+            return(out)
+            }, pattern = map(temp_samdf1_grouped), format="file",  iteration = "vector"),
+
+tar_target(switching_qc,{
+            process <- temp_samdf1_grouped %>%
+            dplyr::group_by(fcid) %>%
+            tidyr::nest() %>%
+            dplyr::mutate(switching_qc = purrr::map(fcid, step_switching_calc, quiet=TRUE))
+            out <- paste0("output/logs/",unique(process$fcid),"/",unique(process$fcid),"_index_switching.pdf")
+            if(is.na(process$switching_qc[[1]]$switch_rate)){
+            pdf(file=out, paper="A4")
+            plot.new()
+            text(x=.5, y=.5, "ERROR: Undetermined reads file not found") 
+            try(dev.off(), silent=TRUE)
+            }
+            return(out)
+            },pattern = map(temp_samdf1_grouped), format="file", iteration = "vector"),
+            
