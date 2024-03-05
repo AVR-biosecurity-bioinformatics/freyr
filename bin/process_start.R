d33e055441fceea0d64e746449b6e4828dc5c9d7
Jack Scanlan <jacksca@gmail.com>
2024-03-05 16:34:34 +1100

    .

--- bin/process_start.R
+++ bin/process_start.R
@@ -1,22 +1,22 @@
 ##### this loads the listed R scripts 
 
-# "projectDir" needs to be defined in the 
+# "projectDir" needs to be defined in the sys.source call
 
-script_list <-
-    c(
-        "jack_notes/.Rprofile",
-        "bin/functions.R",
-        "bin/themes.R",
-        "bin/_targets_packages.R"
-    )
+# script_list <-
+#     c(
+#         "jack_notes/.Rprofile",
+#         "bin/functions.R",
+#         "bin/themes.R",
+#         "bin/_targets_packages.R"
+#     )
 
-print(script_list)
+# print(script_list)
 
 # for (script in script_list) {
 #     source(file.path(projectDir, script))
 # }
 
-# source(file.path(projectDir, "jack_notes/.Rprofile"))
-# source(file.path(projectDir, "bin/functions.R"))
-# source(file.path(projectDir, "bin/themes.R"))
-# source(file.path(projectDir, "bin/_targets_packages.R"))
+source(file.path(projectDir, "jack_notes/.Rprofile"))
+source(file.path(projectDir, "bin/functions.R"))
+source(file.path(projectDir, "bin/themes.R"))
+source(file.path(projectDir, "bin/_targets_packages.R"))
