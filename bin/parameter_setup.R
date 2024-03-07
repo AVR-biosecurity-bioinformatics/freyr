56403174ff63412eae1d49798406e7b03929185b
Jack Scanlan <jacksca@gmail.com>
2024-03-07 13:59:02 +1100

    .

--- bin/parameter_setup.R
+++ bin/parameter_setup.R
@@ -32,7 +32,7 @@ fastqFs <-
 # Filter undetermined reads from sample sheet
 fastqFs <- fastqFs[!str_detect(fastqFs, "Undetermined")]
 
-class(fastqFs)
+write_txt(file = "fastqFs.txt", fastqRs)
 
 # write_csv(file = "fastqFs.csv", x = fastqFs)
 
