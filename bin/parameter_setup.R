bb4bbff7554b8e33f823bad9596558dd2f80fd73
Jack Scanlan <jacksca@gmail.com>
2024-03-07 11:31:03 +1100

    .

--- bin/parameter_setup.R
+++ bin/parameter_setup.R
@@ -7,7 +7,7 @@ print(paste0("data_dir = ", data_dir))
 print(projectDir)
 print(paste(projectDir,data_dir, sep = "/"))
 var1 <- paste0(projectDir,"/",data_dir)
-print(var1)
+var1
 print(paste0("data_dir_abs = ", data_dir_abs))
 runs <- dir(data_dir_abs) # define data directory in module .nf file
 SampleSheet <- list.files(paste0(data_dir_abs,"/", runs), pattern= "SampleSheet", full.names = TRUE) # list all sampleshets
