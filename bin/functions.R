d1a716a2fc1e4eae8df206b9479df20edfc8bd22
Jack Scanlan <jacksca@gmail.com>
2024-03-12 16:58:39 +1100

    .

--- bin/functions.R
+++ bin/functions.R
@@ -448,11 +448,6 @@ step_switching_calc <- function(fcid, barcode_mismatch=1, quiet=FALSE){
       ", Switch rate: ", sprintf("%1.4f%%", res$switch_rate*100),
       ", Contam rate: ", sprintf("%1.6f%%", res$contam_rate*100),
       ", Other reads: ", other_reads)) 
-  
-  if (exists("gg.switch")) { 
-    stop("gg.switch exists") 
-    class(gg.switch)
-    }
 
   pdf(file=paste(fcid,"_index_switching.pdf"), width = 11, height = 8 , paper="a4r")
     plot(gg.switch)
