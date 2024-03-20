3fafdbe52635eac6b4a1e13fffa964eb42bdf1b4
Jack Scanlan <jacksca@gmail.com>
2024-03-20 13:49:25 +1100

    .

--- bin/process_start.R
+++ bin/process_start.R
@@ -1,12 +1,13 @@
-##### this loads the listed R scripts and imports Nextflow variables from params
-
+### load listed R scripts 
 source(file.path(projectDir, "jack_notes/.Rprofile"))
 source(file.path(projectDir, "bin/functions.R"))
 source(file.path(projectDir, "bin/themes.R"))
 source(file.path(projectDir, "bin/_targets_packages.R"))
 
 ### parse Nextflow params dictionary (aka. "params" in Nextflow) directly into R variables
-if (!exists("params_dict")) {stop("'params_dict' not found; check it is defined in .nf")}
+if (!exists("params_dict")) {
+    stop("'params_dict' not found; check it is defined in process .nf file!")
+}
 
 params_list <- params_dict %>% # convert Groovy list format into R nested list
     stringr::str_remove_all("\\[|\\]") %>% 
