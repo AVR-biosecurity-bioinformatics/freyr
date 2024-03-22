b00ff319e9f165ec48f0b81ad3e9d065bb7bbd52
Jack Scanlan <jacksca@gmail.com>
2024-03-22 11:22:28 +1100

    .

--- bin/parameter_setup.R
+++ bin/parameter_setup.R
@@ -26,7 +26,7 @@ runParameters <- list.files(paste0(data_loc_abs,"/", runs), pattern= "[Rr]unPara
 samdf <- create_samplesheet(SampleSheet = SampleSheet, runParameters = runParameters, template = "V4") %>%
 distinct()
 
-stop(" *** stopped manually *** ", status = 1) ##########################################
+stop(" *** stopped manually *** ") ##########################################
 
 # Check that sample_ids contain fcid, if not; attach
 samdf <- samdf %>%
