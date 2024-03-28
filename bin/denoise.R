0af80cd98377c8c921f4f0115813c3cf568cb9ce
Jack Scanlan <jacksca@gmail.com>
2024-03-28 15:50:33 +1100

    .

--- bin/denoise.R
+++ bin/denoise.R
@@ -4,26 +4,37 @@
 
 
 ### run R code
+if (!direction %in% c("forward","reverse")) { 
+    stop(" Input reads direction needs to be set! ")
+}
+
+if (direction == "forward") {
+    direction_short <- "F"
+} else if (direction == "reverse") {
+    direction_short <- "R"
+} else {
+    stop(" Input reads direction needs to be set! ")
+}
 
 # alex function
-step_dada2_single2(
-    fcid, 
-    sample_id, 
-    input_dir,
-    pcr_primers, 
-    output, 
-    qc_dir, 
-    error_model, 
-    read="F",
-    priors = NA, 
-    quiet=FALSE, 
-    multithread=FALSE
-)
+# step_dada2_single2(
+#     fcid, 
+#     sample_id, 
+#     input_dir,
+#     pcr_primers, 
+#     output, 
+#     qc_dir, 
+#     error_model, 
+#     read="F",
+#     priors = NA, 
+#     quiet=FALSE, 
+#     multithread=FALSE
+# )
 
 # direct function from dada2
 dada2::dada(
-    filts, 
-    err = error_model, 
+    derep = reads, 
+    err = errormodel, 
     multithread = FALSE, 
     priors = priors, 
     selfConsist = FALSE, 
