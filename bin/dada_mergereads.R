e36151326e34cd27533fad925ece73a3e94288a0
Jack Scanlan <jacksca@gmail.com>
2024-04-03 14:18:27 +1100

    .

--- bin/dada_mergereads.R
+++ bin/dada_mergereads.R
@@ -3,3 +3,10 @@
 # check variables defined
 
 ### run R code
+seqF <- readRDS(seqF)
+print(seqF)
+
+seqR <- readRDS(seqR)
+print(seqR)
+
+stop(" *** stopped manually *** ") ##########################################
