26450b606818023305acbd22252cdac716691ef5
Jack Scanlan <jacksca@gmail.com>
2024-04-08 10:36:55 +1000

    .

--- bin/filter_seqtab.R
+++ bin/filter_seqtab.R
@@ -199,7 +199,7 @@ cols <- c(`Chimera` = "#9e0142",
 
 gg.abundance <- 
     ggplot2::ggplot(cleanup, aes(x=length, y=log10(Abundance), fill=type)) +
-    geom_bar(stat="identity") + 
+    geom_col() + 
     # scale_y_continuous(trans = pseudo_log_trans(sigma = 1)) + 
     scale_x_continuous(limits=c(min(cleanup$length)-10, max(cleanup$length)+10))+
     theme_bw()+
