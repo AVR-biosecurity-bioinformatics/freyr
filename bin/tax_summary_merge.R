a172b3227e1e6e8e14f7fc16fed72a56f008f509
Jack Scanlan <jacksca@gmail.com>
2024-04-26 17:15:09 +1000

    .

--- bin/tax_summary_merge.R
+++ bin/tax_summary_merge.R
@@ -2,16 +2,21 @@
 
 
 ## check and define variables
-print(tax_summary_list)
+
 tax_summary_list <- # convert Groovy to R list format
     stringr::str_extract_all(tax_summary_list, pattern = "[^\\s,\\[\\]]+") %>% unlist()
-print(tax_summary_list)
+
 tax_summary_list <- lapply(tax_summary_list, readRDS) # read in taxtabs and store as list of tibbles
 
-print(tax_summary_list)
+
 
 ### run R code
+summary_full <- tibble()
+for (i in 1:length(tax_summary_list)){
+    bind_rows(tax_summary_list[i], summary_full)
+}
 
+summary_full
 
 stop(" *** stopped manually *** ") ##########################################
 
