005c6bb341811f33e53d675ce46dc65725356254
Jack Scanlan <jacksca@gmail.com>
2024-04-29 14:35:06 +1000

    .

--- bin/tax_summary_merge.R
+++ bin/tax_summary_merge.R
@@ -11,15 +11,16 @@ tax_summary_list <- lapply(tax_summary_list, readRDS) # read in taxtabs and stor
 
 
 ### run R code
-summary_full <- tibble()
-for (i in 1:length(tax_summary_list)){
-    summary_full <- bind_rows(summary_full, tax_summary_list[i])
+summary_full <- tibble() # create empty tibble
+for (i in 1:length(tax_summary_list)){ # loop through list of tibbles
+    summary_full <- bind_rows(summary_full, tax_summary_list[i]) # combine tibbles into one tibble
 }
 
 summary_full <- summary_full %>%
-    distinct() 
+    distinct() %>%  # remove identical rows, leaving one (ie. duplicate sequences between flow cells)
+    arrange(pcr_primers, OTU_hash) # sort final tibble by pcr_primer and OTU_hash
 
-write_csv(summary_full,"taxonomic_assignment_summary.csv")
+write_csv(summary_full,"taxonomic_assignment_summary.csv") # write to .csv
 
 # stop(" *** stopped manually *** ") ##########################################
 
