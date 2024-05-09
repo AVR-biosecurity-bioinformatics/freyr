ada107b0d95510a5fa26220a375ced0eefb1f338
Jack Scanlan <jacksca@gmail.com>
2024-05-10 09:31:40 +1000

    formatting in READ_TRACKING

--- bin/read_tracking.R
+++ bin/read_tracking.R
@@ -15,14 +15,16 @@
 rt_samples <- # convert Groovy to R list format
     stringr::str_extract_all(rt_samples, pattern = "[^\\s,\\[\\]]+") %>% unlist()
 
+
+### run R code
+
+## format samples-level read tracking tibble 
 samples_tibble <- tibble() # new tibble
-for (i in 1:length(rt_samples)) { # loop through .csv and add values to tibble
+for (i in 1:length(rt_samples)) { # loop through .csv and add values to tibble as new rows
     new_csv <- read_csv(rt_samples[i], show_col_types = F, col_names = F)
     samples_tibble <- rbind(samples_tibble, new_csv)
 }
 
-### TODO: name columns for 'samples_tibble'
+colnames(samples_tibble) <- c("process","sample_id","fcid","pcr_primers","fwd","rev") 
 
-write_csv(samples_tibble, "samples_tibble.csv")
-
-### run R code
+write_csv(samples_tibble, "samples_tibble.csv") # for debugging
