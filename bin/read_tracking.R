84d44ec80340a1611bad757282ffe32c8e485c26
Jack Scanlan <jacksca@gmail.com>
2024-05-09 17:42:19 +1000

    .

--- bin/read_tracking.R
+++ bin/read_tracking.R
@@ -17,7 +17,7 @@ rt_samples <- # convert Groovy to R list format
 
 samples_tibble <- tibble() # new tibble
 for (i in 1:length(rt_samples)) { # loop through .rds files, adding distinct sequences to tibble
-    new_csv <- read_csv(rt_samples, show_col_types = F)
+    new_csv <- read_csv(rt_samples, show_col_types = F, col_names = F)
     samples_tibble <- rbind(samples_tibble, new_csv)
 }
 
